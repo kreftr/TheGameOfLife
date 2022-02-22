84cf0d9427c93158ceb9112175479b639b751b41
s18868 <rafalkreft02@gmail.com>
2022-02-22 23:22:27 +0100

    Added application thumbnail

--- src/main/java/pl/kreft/rafal/thegameoflife/MainApplication.java
+++ src/main/java/pl/kreft/rafal/thegameoflife/MainApplication.java
@@ -3,6 +3,7 @@ package pl.kreft.rafal.thegameoflife;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("thumbnail.png")));
         stage.setTitle("Game of Life");
         stage.setScene(scene);
         stage.show();
